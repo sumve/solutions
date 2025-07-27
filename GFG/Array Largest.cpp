c1b3b537378d35bb678d64874f6aa94a1b66f86b
vermasumedha <sumedhawverma@gmail.com>
2025-07-27 08:30:01 +0530

    Array easy.

---
+++ GFG/Array Largest.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int n = arr.size();
+        int largest = arr[0];
+        for(int i = 0; i < n; i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+            return largest;
+    }
+};
